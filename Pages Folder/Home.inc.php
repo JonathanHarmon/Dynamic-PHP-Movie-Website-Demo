33e3ac0a215c145b1497ca1394c50efca073d40d
JonathanHarmon <60018099+JonathanHarmon@users.noreply.github.com>
2021-05-06 11:39:48 -0400

    Add files via upload

---
+++ Pages Folder/Home.inc.php
@@ -0,0 +1,107 @@
+
+                       
+    <div id="content">
+		<div id="latest"> <p>Latest Entries</p></div>
+		<h1 class="contenthead">Top 10 British Films Of 2021  To Look Out For</h1>
+           <p class="comment">1.01.16 # 23:41 # British Films # One Comment</p>
+          	<div class="contentsmalldiv">
+		    <img style="float: right;" src="newimages/1.jpg" alt=""/>
+		<p>feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar </p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  <h1 class="contenthead">Action Movies 2021 &ndash; Guide</h1><!-- Special Characters https://www.utexas.edu/learn/html/spchar.html -->
+           <p class="comment">1.01.16 # 16:09 # Action Movies # One Comment</p>
+          	<div class="contentsmalldiv">
+		    <img style="float: right;" src="newimages/2.jpg" alt=""/>
+		<p>feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar </p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	   <h1 class="contenthead">Horror Movies 2021  &ndash; Guide</h1>
+           <p class="comment">1.01.16 # 14:05 # Horror Movies # 10 Comments</p>
+          	<div class="contentsmalldiv">
+		    <img style="float: right;" src="newimages/3.jpg" alt=""/>
+		<p>feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar </p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	   <h1 class="contenthead">Comedy Movies 2021 &ndash; Guide</h1>
+           <p class="comment">1.01.16 # 12:06 # Comedy Movies # 6 Comments</p>
+          	<div class="contentsmalldiv">
+		    <img style="float: right;" src="newimages/4.jpg" alt=""/>
+		<p>feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar</p></div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  
+	   <h1 class="contenthead">Star Wars: The Force Awakens &ndash; Review II (Midnight Movie Madness)</h1>
+           <p class="comment">1.01.16 # 11:05 # Review # 4 Comments</p>
+          	<div class="contentbigdiv">
+		    <img style="display: block; margin: 0 auto;" src="newimages/5.jpg" alt=""/>
+		<p style="font-size:1.2em;">feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar</i></strong></p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  <h1 class="contenthead">Top 5 Worst Movies Of 2021</h1>
+           <p class="comment">28.12.15 # 14:27 # Top Ten # 5 Comments</p>
+          	<div class="contentbigdiv">
+		    <img style="display: block; margin: 0 auto;" src="newimages/6.jpg" alt=""/>
+		<p style="font-size:1.2em;">feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar</p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  <h1 class="contenthead">Star Wars: The Force Awakens &ndash; Review (No Spoilers)</h1>
+           <p class="comment">17.12.15 # 4:44 # Review # 16 Comments</p>
+          	<div class="contentbigdiv">
+		    <img style="display: block; margin: 0 auto;" src="newimages/7.jpg" alt=""/>
+		<p style="font-size:1.2em;">feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar</p>
+		</div>
+		<div class="continue" ><a href="starwars.html" target="_blank" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  <h1 class="contenthead">The Hunger Games: Mockingjay Part 2&ndash;  Review (Midnight Movie Madness)</h1>
+           <p class="comment">17.12.15 # 3:50 # Review # No Comment</p>
+          	<div class="contentbigdiv">
+		    <img style="display: block; margin: 0 auto;" src="newimages/8.jpg" alt=""/>
+		<p style="font-size:1.2em;">feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar</p>
+		</div>
+		<div class="continue" ><a href="hunger.html" target="_blank" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  <h1 class="contenthead">What Did You Watch This Week? (Reviews Of Slow West, Mr Holmes, Cooties, The Nightmare, Cub & Howl)</h1>
+           <p class="comment">4.12.15 # 2:27 # Review # 4 Comments</p>
+          	<div class="contentsmalldiv">
+		    <img style="float: right;" src="newimages/9.jpg" alt=""/>
+		<p>feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar</p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  <h1 class="contenthead">Bridge Of Spies &ndash; Review (Midnight Movie Madness)</h1>
+           <p class="comment">3.12.15 # 16:44 # Review # 3 Comments</p>
+          	<div class="contentbigdiv">
+		    <img style="display: block; margin: 0 auto;" src="newimages/10.jpg" alt=""/>
+		<p style="font-size:1.2em;">The outcome of the Cold War lies in the hands of Tom Hanks and his very handsome hat.	 </p>
+		</div>
+		<div class="continue" ><a href="#" style="color: #B0B7CB; text-decoration: none" >continue reading>></a></div>
+	  <div class="clear"></div> <hr>
+	  
+          	<div class="contentbigdiv">
+		    <img style="display: block; margin: 0 auto;" src="newimages/11.jpg" alt=""/>
+	    
+		</div>
+		 <div class="clear"></div>
+	    <h1 class="contenthead">Follow&ndash;Us</h1>
+	    <div id="social">
+		<ul>
+<li style="margin-left: -20px;"><a href="" target="_blank"><img src="images/fb.png" alt="facebook"/></a></li>
+<li><a href=""target="_blank"><img src="images/tweet.png" alt="tweeter"/></a></li>
+<li><a href=""target="_blank"><img src="images/youtube.png" alt="youtube"/></a></li>
+ <li><a href=""target="_blank"><img src="images/link.png" width="110px" height="105px;" alt="linkdIn"/></a></li>
+		</ul>
+	    </div>
+	    
+	    	  <div class="clear"></div>
+		</div>
+	  <div class="clear"></div>
+	 
+	  
+ 	
+         
